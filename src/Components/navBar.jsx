1d527bda9db4c7038df7190fb8460d778a56a5d4
jarc1991 <jesuscontell2020@gmail.com>
2023-06-12 17:06:33 +0200

    arreglando link

--- src/Components/navBar.jsx
+++ src/Components/navBar.jsx
@@ -11,16 +11,14 @@ const NavBar = () => {
 
       <div className='navContainer'>
 
-            <Link to='/'>
+            <Link to='/' className='linkLogo'>
             <h3 className="titleNav">Adele Store</h3>
             </Link>
 
-            <NavLink to={`/categoria/indumentaria`}><button className="btn__nav">Indumentaria</button></NavLink>
-            <NavLink to={`/categoria/discos`}><button className="btn__nav">Discos</button></NavLink>
-            <NavLink to={`/categoria/deportes`}><button className="btn__nav">Deportes</button></NavLink>
-            <NavLink to={`/categoria/box`}><button className="btn__nav">Box</button></NavLink>
-
-                  
+            <NavLink to={`/categoria/indumentaria`} className={({isActive}) => isActive ? 'ActiveOption' : 'Option'}><button className="btn__nav">Indumentaria</button></NavLink>
+            <NavLink to={`/categoria/discos`} className={({isActive}) => isActive ? 'ActiveOption' : 'Option'}><button className="btn__nav">Discos</button></NavLink>
+            <NavLink to={`/categoria/deportes`} className={({isActive}) => isActive ? 'ActiveOption' : 'Option'}><button className="btn__nav">Deportes</button></NavLink>
+            <NavLink to={`/categoria/box`} className={({isActive}) => isActive ? 'ActiveOption' : 'Option'}><button className="btn__nav">Box</button></NavLink>
 
             <CardWid className="Card"/>
 
@@ -34,7 +32,3 @@ const NavBar = () => {
 
 export default NavBar
 
-/*<button className="btn__nav">Indumentaria</button>
-<button className="btn__nav">Discos</button>
-<button className="btn__nav">Deportes</button>
-<button className="btn__nav">Box</button>*/
